b537b383b1d6fffe5b34e62b8b01fbeeb72e410a
Leo Cheung <leozhang621@gmail.com>
2023-05-31 19:12:21 +0800

    init

---
+++ init.lua
@@ -0,0 +1,12 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+Plug 'morhetz/gruvbox'
+Plug 'mhinz/vim-startify'
+Plug 'vim-airline/vim-airline'
+Plug 'preservim/nerdtree'
+vim.call('plug#end')
+
+vim.o.number = true
+vim.o.termguicolors = true
+vim.opt.termguicolors = true
